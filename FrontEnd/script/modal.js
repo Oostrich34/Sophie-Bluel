83de503e131ba82877d9641483821f764267a161
Alexandre Peyrolles <apeyrolles34@gmail.com>
2025-09-25 18:18:06 +0200

    feat: Ajout du formulaire pour envoyer un nouveau projet au back-end dans la modale

--- FrontEnd/script/modal.js
+++ FrontEnd/script/modal.js
@@ -2,7 +2,7 @@
 // Gestion de la modale d'édition
 //==============================
 
-import { projets, genererProjets } from "./gallery.js";
+import { projets, fetchProjets, genererProjets } from "./gallery.js";
 
 export function activerModeEdition() {
     // Barre Mode édition
@@ -110,6 +110,100 @@ export function ouvrirModaleEdition() {
     modal.appendChild(addPhotoBtn);
     overlay.appendChild(modal);
     document.body.appendChild(overlay);
+
+
+    // Ajouter une photo
+    addPhotoBtn.addEventListener("click", () => {
+        // Bouton retour
+        const returnBtn = document.createElement("i");
+        returnBtn.classList.add("fa-solid", "fa-arrow-left", "return-btn");
+
+        // Changement du contenu de la modale pour le formulaire d'ajout
+        const modalGallery = document.querySelector(".modal-gallery");
+        modalGallery.innerHTML = "";
+        container.classList.add("modal-add-photo");
+        modalTitle.textContent = "Ajout photo";
+        const formAddPhoto = document.createElement("form");
+        formAddPhoto.setAttribute("id", "addPhoto");
+
+        // Création des éléments du formulaire d'ajout de photo
+
+        // Prévisualisation image + input file
+        const addImagePreview = document.createElement("div");
+        addImagePreview.classList.add("add-image-preview");
+
+        const imgPreview = document.createElement("i");
+        imgPreview.classList.add("fa-regular", "fa-image", "image-preview");
+
+        const addImageBtn = document.createElement("input");
+        addImageBtn.type = "submit";
+        addImageBtn.classList.add("btn-add-image");
+        addImageBtn.name = "image";
+        addImageBtn.value = "+ Ajouter photo";
+        addImageBtn.accept = "image/png, image/jpeg";
+        addImageBtn.required = true;
+
+        const addImageLabel = document.createElement("label");
+        addImageLabel.htmlFor = "image";
+
+        const imgText = document.createElement("p");
+        imgText.textContent = "jpg, png : 4mo max";
+        
+        addImagePreview.appendChild(imgPreview);
+        addImagePreview.appendChild(imgText);
+        addImagePreview.appendChild(addImageBtn);
+
+        // Ajout du titre
+        const titleLabel = document.createElement("label");
+        titleLabel.textContent = "Titre";
+        titleLabel.setAttribute("for", "title");
+        const titleInput = document.createElement("input");
+        titleInput.type = "text";
+        titleInput.id = "title";
+        titleInput.name = "title";
+        titleInput.required = true;
+        
+        titleLabel.appendChild(titleInput);
+
+        // Sélecteur de catégorie
+        const categorySelect = document.createElement("select");
+        categorySelect.id = "category";
+        categorySelect.name = "category";
+        categorySelect.required = true;
+        const defaultOption = document.createElement("option");
+        defaultOption.value = "";
+        defaultOption.selected = true;
+        const categoryLabel = document.createElement("label");
+        categoryLabel.textContent = "Catégorie";
+        categoryLabel.setAttribute("for", "category");
+        categoryLabel.appendChild(categorySelect);
+        const categories = ["Objets", "Appartements", "Hôtels & restaurants"];
+        categories.forEach(cat => {
+            const option = document.createElement("option");
+            option.value = cat;
+            option.textContent = cat;
+            categorySelect.appendChild(option);
+        });
+        
+        // Bouton valider
+        const addPhotoBtn = document.createElement("input");
+        addPhotoBtn.value = "Valider";
+        addPhotoBtn.type = "button";
+        addPhotoBtn.classList.add("btn-validate");
+
+
+        formAddPhoto.appendChild(addImagePreview);
+        addImagePreview.appendChild(addImageLabel);
+        addImagePreview.appendChild(addImageBtn)
+        formAddPhoto.appendChild(titleLabel);
+        formAddPhoto.appendChild(categoryLabel);
+        categorySelect.appendChild(defaultOption);
+        formAddPhoto.appendChild(addPhotoBtn);
+        modalTitle.prepend(returnBtn);
+        container.appendChild(formAddPhoto);
+        container.appendChild(addPhotoBtn);
+
+    });
 
     // Fermer en cliquant en dehors de la modale
     overlay.addEventListener("click", e => {
